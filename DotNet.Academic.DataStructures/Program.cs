42bd0e0cd2f71299a913d3e211852dcb530f54ac
Walter Lopes <walter_vinicius@outlook.com>
2019-10-05 21:28:26 -0300

    implemented stack

---
+++ DotNet.Academic.DataStructures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DotNet.Academic.DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack.Stack stack = new Stack.Stack();
+
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+
+            stack.Pop();
+
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Size());
+
+            stack.Pop();
+            stack.Pop();
+
+
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Size());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotNet.Academic.DataStructures/Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNet.Academic.DataStructures.Stack
+{
+    public class Stack
+    {
+        public IList<string> Items { get; private set; }
+
+        public Stack()
+        {
+            this.Items = new List<string>();
+        }
+
+
+        public void Push(string value)
+        {
+            Items.Add(value);
+        }
+
+        public void Pop()
+        {
+            var last = this.Size() - 1;
+            Items.RemoveAt(last);
+        }
+
+        public string Peek(string value)
+        {
+            var last = this.Size() - 1;
+            return this.Items[last];
+        }
+
+        public bool IsEmpty()
+        {
+            return !this.Items.Any();
+        }
+
+        public void Clear()
+        {
+            this.Items = new List<string>();
+        }
+
+        public int Size()
+        {
+            return this.Items.Count();
+        }
+    }
+}
