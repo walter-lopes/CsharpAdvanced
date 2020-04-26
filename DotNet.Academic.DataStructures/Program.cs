b3f3a5dd988fbe1988b605babf1d362cd8287e33
Walter Lopes <walter_vinicius@outlook.com>
2020-04-26 11:14:41 -0300

    added tree algos

--- DotNet.Academic.DataStructures/Program.cs
+++ DotNet.Academic.DataStructures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DotNet.Academic.DataStructures.Stack;
 using System;
+using static DotNet.Academic.DataStructures.Trees.BinaryTree;
 
 namespace DotNet.Academic.DataStructures
 {
@@ -7,33 +8,49 @@ namespace DotNet.Academic.DataStructures
     {
         static void Main(string[] args)
         {
-            Stack<string> stack = new Stack<string>();
+            Tree bst = new Tree();
+            Node root = null;
 
-            stack.Push("1");
-            stack.Push("2");
-            stack.Push("3");
+            var arr = new[] { 5, 8, 1, 7, 9, 10, 3, 2 };
 
-            stack.Pop();
-
-            Console.WriteLine(stack.IsEmpty());
-            Console.WriteLine(stack.Size());
-
-            stack.Pop();
-            stack.Pop();
+            for (int i = 0; i < arr.Length; i++)
+            {
+               root = bst.insert(root, arr[i]);
+            }
 
 
-            Console.WriteLine(stack.IsEmpty());
-            Console.WriteLine(stack.Size());
+
+            bst.LevelOrderTraversal(root);
+            Console.ReadKey();
+
+            
+            //Stack<string> stack = new Stack<string>();
+
+            //stack.Push("1");
+            //stack.Push("2");
+            //stack.Push("3");
+
+            //stack.Pop();
+
+            //Console.WriteLine(stack.IsEmpty());
+            //Console.WriteLine(stack.Size());
+
+            //stack.Pop();
+            //stack.Pop();
 
 
-            ProblemWithStack problem = new ProblemWithStack();
-
-            var binary = problem.DivideBy2(10);
-
-            Console.WriteLine(binary);
+            //Console.WriteLine(stack.IsEmpty());
+            //Console.WriteLine(stack.Size());
 
 
-            Console.ReadLine();
+            //ProblemWithStack problem = new ProblemWithStack();
+
+            //var binary = problem.DivideBy2(10);
+
+            //Console.WriteLine(binary);
+
+
+            //Console.ReadLine();
         }
     }
 }
