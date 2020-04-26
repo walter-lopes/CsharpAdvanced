3854d911d36ca01a4433080f659f0a3e72dad18c
Walter Lopes <walter_vinicius@outlook.com>
2020-04-26 11:15:15 -0300

    addec tree algos

---
+++ DotNet.Academic.DataStructures/ArrayTwoD/Array2D.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.Academic.DataStructures.ArrayTwoD
+{
+    public class Array2D
+    {
+        // Complete the hourglassSum function below.
+        static int hourglassSum(int[][] arr)
+        {
+
+            int rows = arr.Length;
+            int columns = arr[0].Length;
+
+            int maxHourGlass = int.MinValue;
+
+            for (int i = 0; i < rows - 3; i++)
+            {
+                for (int j = 0; j < columns - 3; j++)
+                {
+
+                    int currentValue = arr[i][j] + arr[i][j++] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j++] + arr[i+2][j+2];
+
+                    maxHourGlass = Math.Max(currentValue, maxHourGlass);
+                }
+            }
+
+            return maxHourGlass;
+
+        }
+
+        static void Call()
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = hourglassSum(arr);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ DotNet.Academic.DataStructures/Trees/BinaryTree.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.Academic.DataStructures.Trees
+{
+    public class BinaryTree
+    {
+        public class Node
+        {
+            public int value;
+            public Node left;
+            public Node right;
+        }
+
+        public class Tree
+        {
+            public Node insert(Node root, int v)
+            {
+                if (root == null)
+                {
+                    root = new Node();
+                    root.value = v;
+                }
+                else if (v < root.value)
+                {
+                    root.left = insert(root.left, v);
+                }
+                else
+                {
+                    root.right = insert(root.right, v);
+                }
+
+                return root;
+            }
+
+            public void traverse(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+
+                traverse(root.left);
+                traverse(root.right);
+            }
+
+            //Tree: Height of a Binary Tree
+            public int FindMaxDepth(Node root)
+            {
+                if (root is null)
+                {
+                    return 0;
+                } 
+                else
+                {
+                    var ldepth = FindMaxDepth(root.left);
+                    var rdepth = FindMaxDepth(root.right);
+
+                    if (ldepth > rdepth)
+                    {
+                        return ldepth + 1;
+                    }
+                    else
+                    {
+                        return rdepth + 1;
+                    }
+                }          
+            }
+
+            //Tree: Level Order Traversal
+            public void LevelOrderTraversal(Node root)
+            {
+                var queue = new Queue<Node>();
+                var orderTraversalValues = string.Empty;
+
+                queue.Enqueue(root);
+
+                while(queue.Count > 0)
+                {
+                    root = queue.Dequeue();
+
+                    if (root.left != null)
+                        queue.Enqueue(root.left);
+
+                    if (root.right != null)
+                        queue.Enqueue(root.right);
+
+                    orderTraversalValues += root.value.ToString() + " ";
+                }
+
+                Console.WriteLine(orderTraversalValues);
+            }
+
+
+            public void PrintPreOrder(Node root)
+            {
+                if(!(root is null))
+                {
+                    Console.WriteLine(root.value);
+   
+                    PrintPreOrder(root.left);
+
+                    PrintPreOrder(root.right);
+                }                 
+            }
+
+            public void PrintPostOrder(Node root)
+            {
+                if (!(root is null))
+                {
+                    PrintPostOrder(root.left);
+
+                    PrintPostOrder(root.right);
+
+                    Console.WriteLine(root.value);
+                }
+            }
+
+            public void PrintInOrder(Node root)
+            {
+                if (!(root is null))
+                {
+                    PrintInOrder(root.left);
+
+                    Console.WriteLine(root.value);
+
+                    PrintInOrder(root.right);
+                }
+            }
+        }
+    }
+}
